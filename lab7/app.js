67442290a53a93aabfdda20df605fb2a44296338
Denize Ignacio <denizeignacio@gmail.com>
2020-03-21 18:52:10 +0000

    Added header and footer template for .ejs files

--- lab7/app.js
+++ lab7/app.js
@@ -8,11 +8,13 @@ const request = require("request");
 
 // routes
 app.get("/", async function(req, res) {
-    let parsedData = await getImages("otters");
+    let randomKeywords = ["otters", "basketball", "tennis", "soccer", "technology"]
+    let randomIndex = Math.floor(Math.random() * 5)    
+    let parsedData = await getImages(randomKeywords[randomIndex], "all");
     
     // dir displays content of an object
-    console.dir("Parsed Data: " + parsedData);
-    res.render("index", {"image":parsedData.hits[0].largeImageURL});
+    // console.dir("Parsed Data: " + parsedData);
+    res.render("index", {"image":parsedData});
     
 });
 
@@ -20,19 +22,25 @@ app.get("/results", async function(req, res) {
     // console.dir(req);
     // gets the keyword user search for from input text
     let keyword = req.query.keyword;
-    let parseData = await getImages(keyword);
-    res.render("results", {"images":parseData});
+    let orientation = req.query.orientation;
+    let parseData = await getImages(keyword, orientation);
+    let numbers = randomNumbers();
+    console.log(numbers)
+    
+    res.render("results", {
+                            "images":parseData,
+                            "indexes":numbers   
+                           });
 });
 
 // returns all data from Pixabay API as a JSON object
-function getImages(keyword) {
-    
+function getImages(keyword, orientation) {
+    // by default, orientation is set to "all"
     return new Promise(function(resolve, reject){ 
-        request('https://pixabay.com/api/?key=15448805-2fb6d362dc06889b4a4ad4a80&q=' + keyword, function (error, response, body) {
+        request('https://pixabay.com/api/?key=15448805-2fb6d362dc06889b4a4ad4a80&q=' + keyword + '&orientation=' + orientation, function (error, response, body) {
             if(!error && response.statusCode == 200) {
                 // need to parse the body because its contents is a string; conver to a JSON object 
                 let parseData = JSON.parse(body);
-                
                 // returns ParseData in root route
                 resolve(parseData); 
                 // let index = Math.floor(Math.random() * parseData.hits.length);
@@ -45,6 +53,19 @@ function getImages(keyword) {
             }
         }); // request    
     });
+}
+
+function randomNumbers() {
+    let numbers = [];
+    let index = 0
+    while(index < 4) {
+        let number = Math.floor(Math.random() * 20);
+        if(!numbers.includes(number)) {
+          numbers.push(number)
+          index++;
+        }
+    }
+    return numbers;
 }
 
 
